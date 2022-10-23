806672254efbf8146fc9777b63e92324dd9505c9
pwnd27 <thebuff27@mail.ru>
2022-10-23 15:09:17 +0700

    yep

--- app/my_project/eat/views.py
+++ app/my_project/eat/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def to_eat(request):
-    return HttpResponse('<h1>Что покушать</h1>')
+    return render(request, 'eat/index.html')
